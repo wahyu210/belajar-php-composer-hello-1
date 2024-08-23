00587ef56c67c452ee8b128674f5e152521ebbcf
Mochamad Wahyu <wahyu212122@gmail.com>
2024-08-23 10:01:47 +0700

    conth library sederhana composer

---
+++ src/Customer.php
@@ -0,0 +1,10 @@
+<?php
+namespace MochamadWahyu\Belajar;
+class Customer{
+    public function __construct(private string $name){
+
+    }
+    public function sayHello(string $name){
+        return "hello $name, my name is $this->name";
+    }
+}
